acc86d43b9345c74181ae5bfc7e4f9c9f82ae4cf
João Elvas <joao.elvas@gmail.com>
2018-04-12 17:49:15 +0100

    v2

--- 03_transitions/src/components/Transition.js
+++ 03_transitions/src/components/Transition.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
+import Transition from 'react-transition-group/Transition'
+
 import '../css/App.css';
 
 class TransitionComp extends Component{
 
+    state = {
+        show: true
+    }
+
+    showDiv = () => {
+        this.setState({
+            show: !this.state.show ? true : false
+        })
+    }
+
     render(){
         return(
             <div>
-                Transition
+                <Transition 
+                    in={this.state.show}
+                    timeout={2000}
+                >
+                    { state => <p> {state} </p>}
+                </Transition>
+                <div className="showDiv" onClick={this.showDiv}>
+                    Show or hide
+                </div>
             </div>
-
         )
     }
 }
