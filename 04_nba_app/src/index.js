a9b537709034bb010e25632139ab509d3c31e089
João Elvas <joao.elvas@gmail.com>
2018-04-17 16:45:07 +0100

    Beginning lecture 48

---
+++ 04_nba_app/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
+
+import Routes from './routes'
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Routes/>
+        </BrowserRouter>
+    );
+};
+
+export default App;
+
+ReactDOM.render(<App />, document.getElementById('root'));
