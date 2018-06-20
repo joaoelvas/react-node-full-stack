27f2705880f2c55036413980c59a3b440dc81257
João Elvas <joao.elvas@gmail.com>
2018-06-20 15:48:12 +0100

    End of lecture 104

---
+++ 06_redux/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+const App = () => (
+    <div>
+        Hello
+    </div>
+)
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware()}>
+        <App />
+    </Provider>    
+    
+, document.getElementById('root'));
